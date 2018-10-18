6e91c747571fe8b3cd6dc8ea5359cf70096dffc2
Jeroen ter Heerdt <jeroen_ter_heerdt@hotmail.com>
2018-10-18 15:46:31 +0200

    Adding test file for relay

---
+++ relaytest.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+pins = [2, 3, 4, 17]
+for pin in pins:
+	GPIO.setup(pin, GPIO.OUT)
+	GPIO.output(pin, GPIO.HIGH)
+print("All pins set to high")
+while 1:
+	inp = input("Enter pin number (2,3,4 or 17) and high or low: ")
+	inpslit = inp.split(' ')
+	to = GPIO.HIGH
+	if inpslit[1] == 'low':
+		to = GPIO.LOW
+	GPIO.output(int(inpslit[0]),to)
